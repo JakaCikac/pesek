8753e2f114d03509b458e08a2feea899381039d2
JakaCikac <nanorax@gmail.com>
2014-11-16 14:18:10 +0100

    Moving the player with arrow keys.

--- js/Game.js
+++ js/Game.js
@@ -8,19 +8,19 @@ CatchMice.Game.prototype = {
       
       //set world dimensions
       this.game.world.setBounds(0, 0, 1920, 1920);
-      this.background = this.game.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'space');
+      this.background = this.game.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'background');
       
       //create player
-      this.player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'playership'); 
+      this.player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player'); 
       this.player.scale.setTo(2);
-      this.player.animations.add('fly', [0, 1, 2, 3], 5, true);
-      this.player.animations.play('fly');
+      //this.player.animations.add('fly', [0, 1, 2, 3], 5, true);
+     // this.player.animations.play('fly');
       
       //the camera will follow the player in the world
       this.game.camera.follow(this.player);
       
-      this.generateAsteriods();
-      this.generateCollectables();
+      //this.generateAsteriods();
+      //this.generateCollectables();
       
       //player initial score of zero
       this.playerScore = 0;
@@ -30,20 +30,62 @@ CatchMice.Game.prototype = {
       
       //enable player physics
       this.game.physics.arcade.enable(this.player);
-      this.playerSpeed = 120;
+      this.playerSpeed = 240;
       this.player.body.collideWorldBounds = true;
       
+      // create the ability to control our player with the keyboard
+      this.cursor = this.game.input.keyboard.createCursorKeys();
+
+      
       //sounds
-      this.explosionSound = this.game.add.audio('explosion');
-      this.collectSound = this.game.add.audio('collect');
+      //this.explosionSound = this.game.add.audio('explosion');
+      //this.collectSound = this.game.add.audio('collect');
     
       
   },
+    
+    // ----------------------------
+    // Moving our player with the keyboard
+    // ----------------------------
+    movePlayer: function() {
+        // If the left arrow key is pressed
+        if (this.cursor.left.isDown) {
+            // Move the player to the left
+            this.player.body.velocity.x = -200;
+       }
+
+       // If the right arrow key is pressed
+       else if (this.cursor.right.isDown) {
+           // Move the player to the right
+           this.player.body.velocity.x = 200;
+       }
+
+       // If neither the right or left arrow key is pressed
+       else {
+           // Stop the player
+           this.player.body.velocity.x = 0;
+       }
+
+       // If the up arrow key
+       if (this.cursor.up.isDown) { 
+           // Move player up
+           this.player.body.velocity.y = -200;
+           
+       // If the down arrow key is pressed
+       } else if (this.cursor.down.isDown) {
+           // Move player down
+           this.player.body.velocity.y = +200;
+       }      
+        
+    },
+    
   update: function() {
       if(this.game.input.activePointer.justPressed()) {
           //move on the direction of the input
           this.game.physics.arcade.moveToPointer(this.player, this.playerSpeed);
     }
+      
+    this.movePlayer();
       
     //collision between player and asteroids
      this.game.physics.arcade.collide(this.player, this.asteroids, this.hitAsteroid, null, this);
@@ -79,7 +121,7 @@ generateAsteriods: function() {
     
 hitAsteroid: function(player, asteroid) {
     //play explosion sound
-    this.explosionSound.play();
+    //this.explosionSound.play();
 
     //make the player explode
     var emitter = this.game.add.emitter(this.player.x, this.player.y, 100);
